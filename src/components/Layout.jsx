c7600c8905d1bbbb33e605068bf6c11153f64ef8
Alejandro Cortes <alejandro.cortes@ironbit.com.mx>
2019-10-14 15:49:57 -0500

    Fixed Now reactstrap works

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <div className='App'>
     <Header />
-    {children}
+    <Container>
+      <Row>
+        <Col>
+          {children}
+        </Col>
+      </Row>
+    </Container>
     <Footer />
   </div>
 );
