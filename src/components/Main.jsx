9a9894234af852d20f84545a0e30684328c4df19
Alejandro Cortez <ziker@live.com.mx>
2019-10-25 10:16:04 -0500

    Functionality in Directions almost complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Alert, Col, Row } from 'reactstrap';
+import { Alert } from 'reactstrap';
 import fireApp from '../initializers/firebase';
 import Map from './Map';
 import { setName } from '../reducers';
 
 class Main extends Component {
   componentDidMount() {
-    const databaseRef = fireApp.database().ref().child('users').child('name');
-    databaseRef.on('value', (snapshot) => {
-      console.log(snapshot.val());
-      this.props.setName(snapshot.val());
-    });
+    this.props.setName('Alex');
+    /*  const databaseRef = fireApp.database()
+        .ref()
+        .child('users')
+        .child('name');
+      databaseRef.on('value', (snapshot) => {
+        console.log(snapshot.val());
+        this.props.setName(snapshot.val());
+      });*/
   }
 
   render() {
     return (
       <>
-        <Row>
-          <Col md='3'>
-            <Alert color='dark' className='mt-3'>
-              Hola de nuevo
-              {' '}
-              {this.props.name}
-              {' '}
-              {}
-            </Alert>
-          </Col>
-          <Col md='9'>
-            <Map />
-          </Col>
-        </Row>
+        <Alert color='dark' className='mt-3'>
+          Hola de nuevo
+          {' '}
+          {this.props.name}
+          {' '}
+          {}
+        </Alert>
+        <Map />
       </>
     );
   }
