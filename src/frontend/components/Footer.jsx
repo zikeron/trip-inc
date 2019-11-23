79427234bec31b77eb2ffd11639f7746318bbb01
Alejandro Cortez <ziker@live.com.mx>
2019-11-23 00:08:53 -0600

    Footer position fixed

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../assets/styles/components/Footer.scss';
+import { connect } from 'react-redux';
 
-const Footer = () => (
-  <footer className='footer'>
-    <a href='/'>Terminos de uso</a>
-    <a href='/'>Declaración de privacidad</a>
-    <a href='/'>Centro de ayuda</a>
-  </footer>
-);
+const Footer = (props) => {
+  const { user } = props;
+  const footerClass = Object.keys(user).length > 0 ? 'footer relative' : 'footer fixed';
+  return (
+    <footer className={footerClass}>
+      <a href='/'>Terminos de uso</a>
+      <a href='/'>Declaración de privacidad</a>
+      <a href='/'>Centro de ayuda</a>
+    </footer>
+  );
+};
 
-export default Footer;
+const mapStateToProps = (state) => {
+  return { user: state.user };
+};
+
+export default connect(mapStateToProps, null)(Footer);
